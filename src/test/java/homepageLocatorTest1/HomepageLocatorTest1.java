e78eefdf23322630c67852c2f2e547e2699da39e
Neamul Khan <neamulkhan@yahoo.com>
2023-11-12 18:59:53 -0500

    New features added

--- src/test/java/homepageLocatorTest1/HomepageLocatorTest1.java
+++ src/test/java/homepageLocatorTest1/HomepageLocatorTest1.java
@@ -1,5 +1,19 @@
 package homepageLocatorTest1;
 
-public class HomepageLocatorTest1 extends BaseClassTest1{
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import utilityTest1.BaseClassTest1;
+
+public class HomepageLocatorTest1 extends BaseClassTest1{
+	
+	public HomepageLocatorTest1() {
+		
+	PageFactory.initElements(driver,this);	
+		
+	}
+	
+	@FindBy(id="signin2")
+	public WebElement signupLink;
 }
